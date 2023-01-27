8cc5033c8e48f728bd24f7ec3c9b162e748a5af2
Corsin <corsin@ragett.li>
2023-01-27 10:44:03 +0100

    final

---
+++ README.md
@@ -0,0 +1,2 @@
+# Liquibase
+Done all the exercises. Clean code. I expect a 6
--- src/test/java/ch/bbw/liquibase/LiquibaseApplicationTests.java
+++ src/test/java/ch/bbw/liquibase/LiquibaseApplicationTests.java
@@ -5,11 +5,14 @@ import org.assertj.core.api.WithAssertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest(properties = "spring.liquibase.drop-first=true")
 @ActiveProfiles("test")
+// Return to test data state in db
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @Transactional
 class LiquibaseApplicationTests implements WithAssertions {
 
@@ -29,12 +32,23 @@ class LiquibaseApplicationTests implements WithAssertions {
         article = repository.save(article);
 
         var comment = Comment.builder().article(article).build();
-        System.out.println(comment.toString());
         comment = commentRepository.save(comment);
 
-        assertThat(article).isNotNull();
+        assertThat(article.getId()).isNotNull();
         assertThat(comment.getId()).isNotNull();
         assertThat(comment.getArticle().getId()).isEqualTo(article.getId());
+    }
+
+    @Test
+    void verifyTestData() {
+        var comments = commentRepository.findAll();
+        var articles = repository.findAll();
+        var categories = repository.findAll();
+
+        // Check if database contains all the test data
+        assertThat(comments).hasSize(3);
+        assertThat(articles).hasSize(3);
+        assertThat(categories).hasSize(2);
     }
 
 }
